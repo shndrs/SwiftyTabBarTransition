67947834c032e9a45b60566117dfd30355ecfb93
NP2 <sahandraeisi1994@gmail.com>
2019-05-25 09:39:58 +0430

    Initial project setup

---
+++ SwiftyTabBarTransitionExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyTabBarTransitionExamples
+//
+//  Created by NP2 on 5/25/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
